f2757da5fcb029ebd04f5d4aad0cc445be3ca7d8
Shukri Senawi <shukrisenawi@users.noreply.github.com>
2026-05-25 09:19:24 +0800

    exclude no ahli route from csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+            'carian-pemilih/update-no-ahli',
+        ]);
+
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
