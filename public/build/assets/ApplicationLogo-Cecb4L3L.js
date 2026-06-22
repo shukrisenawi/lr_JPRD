8765842936c86136b0e85420badd8afe10ed06d8
Shukri Senawi <shukrisenawi@users.noreply.github.com>
2026-06-22 10:27:28 +0800

    detail modal no_rumah grey sahaja

--- public/build/assets/ApplicationLogo-Cecb4L3L.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-BnwKRn4Q.js";const r="/build/assets/logo-BVbxA-Un.png";function g({className:o="",alt:s="Logo",...t}){return n.jsx("img",{...t,src:r,alt:s,className:o})}export{g as A};
