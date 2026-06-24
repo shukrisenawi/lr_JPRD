c3c135dff2b068785bc21dbd0dd576b88c4ac07f
Shukri Senawi <shukrisenawi@users.noreply.github.com>
2026-06-24 12:12:26 +0800

    Sembunyi menu Senarai AJK UDM untuk peringkat JPRD dan Cawangan

--- public/build/assets/ApplicationLogo-BzLnRhth.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-BjR7wMfg.js";const r="/build/assets/logo-BVbxA-Un.png";function g({className:o="",alt:s="Logo",...t}){return n.jsx("img",{...t,src:r,alt:s,className:o})}export{g as A};
